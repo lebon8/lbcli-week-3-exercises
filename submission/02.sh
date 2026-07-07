7c3f8534200528a576e72b81689d23f3adf4dceb
lebon8 <drnlebonenian@gmail.com>
2026-07-08 01:26:48 +0200

    Treaty

--- submission/02.sh
+++ submission/02.sh
@@ -1,1 +1,5 @@
 # Create a native segwit address and get the public key from the address.
+
+Address=$(bitcoin-cli -regtest -rpcwallet=builderswallet getnewaddress "" bech32)
+
+bitcoin-cli -regtest getaddressinfo $Address | jq -r '.pubkey'
