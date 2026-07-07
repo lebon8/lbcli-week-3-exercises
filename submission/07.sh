b9e95fec81fa84f3a5da719df90001a6da5317a2
lebon8 <drnlebonenian@gmail.com>
2026-07-08 01:36:46 +0200

    Treaty

--- submission/07.sh
+++ submission/07.sh
@@ -3,4 +3,4 @@
 
 transaction=cHNidP8BAHsCAAAAAhuVpgVRdOxkuC7wW2rvw4800OVxl+QCgezYKHtCYN7GAQAAAAD/////HPTH9wFgyf4iQ2xw4DIDP8t9IjCePWDjhqgs8fXvSIcAAAAAAP////8BigIAAAAAAAAWABTHctb5VULhHvEejvx8emmDCtOKBQAAAAAAAAAA
 
-bitcoin-cli -regtest decodepsbt $transaction | jq -r '[.tx.vout[].value]|add *10000000'
+bitcoin-cli -regtest decodepsbt $transaction | jq -r '[.tx.vout[].value]|add *100000000'
